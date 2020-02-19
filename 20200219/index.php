56f278ac3895de29b6a46b602cfa1883936c927a
Amaliamelo <amaliamelovitoria@gmail.com>
2020-02-19 14:04:14 -0300

    EX20200219

---
+++ 20200219/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html >
+  <head>
+    <meta charset="utf-8">
+    <title>BarbeariaIFSP</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"  href="css/css.css" />
+
+  </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto menu ">
+    <div class="inner">
+      <h3 class="masthead-brand">BarbeariaIFSP</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="index.php">Home</a>
+        <a class="nav-link" href="form_agendamento.php">Agendar</a>
+        <a class="nav-link" href="lista_agendamento.php">Lista de Agendamentos</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <p ><h1 class="cover-heading home ">Seja Bem-Vindo!!</h1></p>
+    <p class="lead text">Marque seu hoarario conosco.</p>
+    <p class="lead">
+      <a href="form_agendamento.php" class="btn btn-lg btn-light">Agendar</a>
+    </p>
+  </main>
+
+   <footer class="footer fixed-bottom text-center">
+    <div class="inner">by @AmaliaMelo.
+    </div>
+  </footer>
+</div>
+        <script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/validaform.min.js"></script>
+</body>
+</html>
---
+++ 20200219/recebe_agendamento.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BarbeariaIFSP</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"  href="css/css.css"/>
+
+  </head>
+  <body>
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto menu text-center">
+    <div class="inner">
+      <h3 class="masthead-brand">BarbeariaIFSP</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="index.php">Home</a>
+        <a class="nav-link" href="form_agendamento.php">Agendar</a>
+        <a class="nav-link" href="lista_agendamento.php">Lista de Agendarmentos</a>
+      </nav>
+    </div>
+  </header>
+    <div class="text-center">
+        <?php
+            include "funcoes.inc";
+            agendar();
+        ?>
+    </div>
+    
+    <footer class="footer fixed-bottom text-center">
+    <div class="inner">by @AmaliaMelo.
+    </div>
+  </footer>
+    <script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/validaform.min.js"></script>
+</body>
+</html>
